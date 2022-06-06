ec25504fe3bfe17a5c3e7a54a879a3b392eafbd7
MononobenoFuto <57579667+MononobenoFuto@users.noreply.github.com>
2022-06-06 21:29:05 +0800

    20220606
    临时上传用于测试

--- src/test/java/com/nya/service/UserServiceTest.java
+++ src/test/java/com/nya/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.nya.service;
 
+import com.nya.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +13,17 @@ public class UserServiceTest {
     @Test
     void testGetById() {
         System.out.println(userService.getById(2019112297));
+    }
+
+    @Test
+    void testInsert() {
+        User user = new User();
+        user.setNickname("testnick");
+        user.setIssuper(0);
+        user.setPassword("testpwd");
+        user.setPhone("13777777777");
+        user.setSex("m");
+        user.setId(2019112222);
+        userService.save(user);
     }
 }
