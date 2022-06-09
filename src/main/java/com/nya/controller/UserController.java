4f6e9a6b8efed9817d8b27350923b84b372d0fa4
MononobenoFuto <57579667+MononobenoFuto@users.noreply.github.com>
2022-06-09 11:00:08 +0800

    添加了登录后端接口

--- src/main/java/com/nya/controller/UserController.java
+++ src/main/java/com/nya/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.nya.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.nya.domain.User;
 import com.nya.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -26,6 +28,11 @@ public class UserController {
     @PostMapping
     public Boolean save(@RequestBody User user) {
         return userService.save(user);
+    }
+    @PostMapping("/login")
+    public Integer login(@RequestBody Map<String, String> map) {
+        System.out.println(map.get("phone"));
+        return userService.login(map.get("phone"), map.get("password"));
     }
 
 //    @PutMapping
